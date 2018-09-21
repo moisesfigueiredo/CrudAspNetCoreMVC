f20f544a8ee74ae75713dffa6a524acbf563439f
moisesfigueiredo <moises.figueiredo@gmail.com>
2018-09-21 18:48:54 -0300

    Publicação inicial

---
+++ CrudMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CrudMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+           
+        }
+    }
+}
---
+++ CrudMVC/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudMVC.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
